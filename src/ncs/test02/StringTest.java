96a590f64888061229c6235bb550622465c66cf1
kitoku <kitoku@DESKTOP-RUJP58R>
2025-06-03 19:41:32 +0900

    20250603 ADD

--- src/ncs/test02/StringTest.java
+++ src/ncs/test02/StringTest.java
@@ -1,23 +1,26 @@
 package ncs.test02;
 
 public class StringTest {
-public static void main(String args[]) { 
-	String str = "1.22,4.12,5.93,8.71,9.34";
-	double data[]= new double[5];
-	double sum = 0;
-	// str에서  데이터를  분리한다. 
-	String[] st;
-	st=str.split(",");
-	data[] = (double)st[i];
-	
-	for(int i=0; i<data.length;i++) {
-		data[i] = (double)st[i];
-	}return data[];
-//for~each 문  사용한다. 
-	// 배열에  실수  데이터를  넣는다.
+    public static void main(String args[]) {
+        // 문자열 데이터 준비
+        String str = "1.22,4.12,5.93,8.71,9.34";
 
-//배열  데이터의  합을  구한다. 
-	}  
-// 결과  값을  출력  한다.   
-} 
+        // 배열에 실수 데이터를 넣는다.
+        String[] st = str.split(",");         // str에서 데이터를 분리한다.
+        double data[] = new double[st.length];
+        for (int i = 0; i < st.length; i++) {
+            data[i] = Double.parseDouble(st[i]);
+        }
+
+        // 배열 데이터의 합을 구한다.
+        double sum = 0;
+        for (double num : data) {             // for-each문 사용
+            sum += num;
+        }
+
+        // 결과 값을 출력한다.
+        System.out.printf("합계 : %.2f\n", sum);
+        System.out.printf("평균 : %.2f\n", sum / data.length);
+    }
+}
 
--- src/ncs/test03/DateTest.java
+++ src/ncs/test03/DateTest.java
@@ -1,24 +1,24 @@
 package ncs.test03;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class DateTest {
-	public static void main(String[] args) {
-		Calendar cal = Calendar.getInstance();
-		
-		int year= cal.get(Calendar.YEAR);
-		int month=cal.get(Calendar.MONTH)+1;
-		int day=cal.get(Calendar.DATE);
-		int dayOfWeek=cal.get(Calendar.DAY_OF_WEEK);
-		
-		
-		
-		
-	
-	
-	
+    public static void main(String[] args) {
+        GregorianCalendar cal = new GregorianCalendar(1987, 4, 27);  // 생일 (5월은 4로 입력)
+        GregorianCalendar calToday = new GregorianCalendar();         // 오늘 날짜
 
-	}//end main()
+        SimpleDateFormat fm = new SimpleDateFormat("yyyy년 M월 d일 E요일");
 
+        // 생일 출력
+        System.out.println("생일 : " + fm.format(cal.getTime()));
+
+        // 현재 날짜 출력
+        System.out.println("현재 : " + fm.format(calToday.getTime()));
+
+        // 나이 계산 (한국식 만나이 = 올해 - 태어난 해 + 1)
+        int age = calToday.get(Calendar.YEAR) - cal.get(Calendar.YEAR) + 1;
+        System.out.println("나이 : " + age + "세");
+    }
 }
